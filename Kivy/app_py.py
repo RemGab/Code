33ca9602903f23dc7df6c3583cd30de72767d6e2
RemGab <71643572+RemGab@users.noreply.github.com>
2022-01-12 13:40:55 +0100

    'mis à jour pro'

--- Kivy/app_py.py
+++ Kivy/app_py.py
@@ -1,9 +1,9 @@
 from typing import Counter
 from kivy.app import App
-from kivy.metrics import  dp
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
@@ -12,12 +12,32 @@ from kivy.uix.stacklayout import StackLayout
 
 class WidgetExample(GridLayout):
     count = 1
+    count_enabled = BooleanProperty(False)
     my_text = StringProperty("1")
     
     def on_button_click(self):
         print('Button clicked')
-        self.count += 1
-        self.my_text = str(self.count)
+        if self.count_enabled :
+            self.count += 1
+            self.my_text = str(self.count)
+
+
+    def on_toggle_button_state(self,widget):
+        print('Toggle state :'+ widget.state)
+        if widget.state == 'normal':
+            widget.text= 'OFF'
+            self.count_enabled = False
+        else:
+            widget.text = 'ON'
+            self.count_enabled = True
+    
+    def on_switch_active(self,widget):
+        print('Switch: '+ str(widget.active))
+
+    def on_slider_value(self,widget):
+        print('Slider: ' + str(int(widget.value)))
+
+
         
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
