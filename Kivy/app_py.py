74b0bf2a78df4c21976e26ffe5005b4114b0a898
RemGab <71643572+RemGab@users.noreply.github.com>
2022-01-16 15:58:11 +0100

    mooving part

--- Kivy/app_py.py
+++ Kivy/app_py.py
@@ -1,3 +1,6 @@
+import imp
+from msilib.schema import SelfReg
+from turtle import circle, width
 from typing import Counter
 from kivy.app import App
 from kivy.metrics import dp
@@ -8,7 +11,11 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
-
+from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Ellipse
+from kivy.properties import Clock
 
 class WidgetExample(GridLayout):
     count = 1
@@ -42,8 +49,6 @@ class WidgetExample(GridLayout):
     def on_text_validate(self,widget):
         self.text_input_str = widget.text
         
-
-        
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -54,9 +59,6 @@ class StackLayoutExample(StackLayout):
             b= Button(text=str(i+1),size_hint=(None,None),size=(dp(100),dp(100)))
             self.add_widget(b)
     pass
-
-# class GridLayoutExample(GridLayout):
-#     pass
 
 class AnchorLayoutExample(AnchorLayout):
     pass
@@ -73,14 +75,71 @@ class BoxLayoutExample(BoxLayout):
     #     self.add_widget(b2)
     #     self.add_widget(b3)
         
-        
-    
 class MainWidget(Widget):
     pass
 
 class TheLabApp(App):
     pass
 
+class CanvasExample5(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size = dp(50)
+        self.vx = dp(3)
+        self.vy = dp(3)
+        with self.canvas:
+            self.ball = Ellipse(pos=self.center,size=(self.ball_size ,self.ball_size ))
+        Clock.schedule_interval(self.update,1/60)
+    def on_size(self, *args):
+        # print("on size : " + str(self.width) +"," + str(self.height))
+        self.ball.pos = (self.center_x-self.ball_size/2,self.center_y-self.ball_size/2)
+    def update(self, dt):
+        # print("update")
+        x, y = self.ball.pos
+        x += self.vx
+        y += self.vy
+        
+
+        if y + self.ball_size > self.height:
+            y = self.height - self.ball_size
+            self.vy  = -self.vy
+            
+        if x + self.ball_size > self.width:
+            x = self.width - self.ball_size
+            self.vx  = -self.vx
+
+        if y < 0 :
+            y = 0
+            self.vy = - self.vy
+        
+        if x < 0 :
+            x =0
+            self.vx= -self.vx
+
+        self.ball.pos = (x,y)
+        
+
+class CanvasExample4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100,100,400,500), width = 2)
+            Color(0,1,0)
+            Line(circle=(400,200,88),width=2)   
+            Line(rectangle=(700,500,150,100),width=2)
+            self.rect = Rectangle(pos=(100,100),size=(150,100))  
+    def on_button_a_click(self):
+        x, y = self.rect.pos
+        w ,h = self.rect.size
+        inc = dp(10)
+
+        diff = self.width - (x+w)
+        if diff < inc : 
+            inc = diff
+
+        x += inc  
+        self.rect.pos =(x,y)     
+    
 class CanvasExample3(Widget):
     pass
 
