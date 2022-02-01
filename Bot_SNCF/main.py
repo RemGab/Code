6caeb868da442b2141025f521dc8c4cd649509c1
RemGab <71643572+RemGab@users.noreply.github.com>
2022-02-01 08:35:52 +0100

    Update main.py

--- Bot_SNCF/main.py
+++ Bot_SNCF/main.py
@@ -1,24 +1,41 @@
 import time
+from click import option
 from cv2 import waitKey
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
 
-driver = webdriver.Chrome('Bot_SNCF\Ressources\chromedriver.exe')  # Optional argument, if not specified will search path.
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=1100,1000")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent={userAgent}')
 
-driver.get('http://www.google.com/')
+driver = webdriver.Chrome(options=options, executable_path='Bot_SNCF\Ressources\chromedriver.exe')  # Optional argument, if not specified will search path.
+
+driver.get('https://www.sncf-connect.com/app/account')
 
 wait = WebDriverWait(driver, 10)
-element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="bySelection"]/div[3]')))
-element.click()
-time.sleep(3) # Let the user actually see something!
-driver.switch_to.alert().dismiss()
+try:
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="bySelection"]/div[3]')))
+    element.click()
+except:
+    pass
 
+try:
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="L2AGLb"]/div')))
+    element.click()
+except:
+    pass
 
-
-
-
+time.sleep(10)
 
 driver.quit()
