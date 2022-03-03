89c2a3299e202ced9d1da6c24760a33a027e66d9
RemGab <71643572+RemGab@users.noreply.github.com>
2022-03-03 08:09:58 +0100

    daa

--- Game/test1.py
+++ Game/test1.py
@@ -6,12 +6,28 @@ WHITE = [255, 255, 255]
 GREEN = [0, 255, 0] 
 RED = [255, 0, 0] 
 BLUE = [0 , 0 , 255] 
- 
+path = r'Game\ressource'
 # initialisation de l'écran de jeu 
 pygame.init() 
- 
-# Initialise la fenêtre de jeu 
-TailleEcran = [400, 300] 
-screen = pygame.display.set_mode(TailleEcran) 
 
+TailleEcran = [300, 200] 
+ecran = pygame.display.set_mode(TailleEcran)
+
+# pygame.display.set_caption("Mon super titre de Ouf !")
+
+image = pygame.image.load(path + "\image.png").convert_alpha()
+# pygame.display.set_icon(image)
+
+# Initialise la fenêtre de jeu 
+
+
+
+continuer = True
+
+while continuer :
+    ecran.blit(image,(0,50))
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            continuer = False
+    pygame.display.flip()
 pygame.quit()
